cedcdaf39534d9d675f6c830000894b17598105c
AlinaDbeep <82227705+AlinaDbeep@users.noreply.github.com>
2021-12-17 20:41:50 +0200

    Update main.py
    made it a bit more complicated. it's possible to rerun or close the app. if you enter symbols outside of the alphabet, the app asks you to enter the correct characters again

--- main.py
+++ main.py
@@ -8,13 +8,28 @@ dict = {
 
 #print(dict)
 
-word = input("Enter a word: ").upper()
-word_list = [dict[letter] for letter in word]
-print(" ")
-index = 0
-for letter in word_list:
-    print(f"{word[index]} as in {word_list[index]}")        
-    index +=1
-        
+def app():
+    app_off = False
+    while not app_off:
+        word = input("Enter a word: ").upper()
+        try:
+            word_list = [dict[letter] for letter in word]
+            print(" ")
+            index = 0
+            for letter in word_list:
+                print(f"{word[index]} as in {word_list[index]}")
+                index +=1
+            print("")
+            again = input("Do you want to use the app one more time?\nType 'y' to continue or 'no' to exit: ")
+            if again == 'y':
+                app()
+            else:
+                print("\nSee you! Have a nice day!")
+                quit()
+        except KeyError:
+            print("Sorry, only letters in the alphabet, please")
+            app()
 
+
+app()
 
